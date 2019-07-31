86297ac4143f5f1b6805a251d8f5884ca82a4c77
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-31 16:06:30 +0000

    more CLI work, need to fix loop for CLI flow

--- lib/Park_Trails/CLI.rb
+++ lib/Park_Trails/CLI.rb
@@ -6,13 +6,27 @@ def initialize
 end
 
 def county_trails
+ 
   puts "Here is the list of PBC trails:"
   puts "trail 1"
   puts "trail 2"
   puts "trail 3"
   puts "trail 4"
   puts "Please type the number of the trail you would like to know more about, or type exit."
-    input = gets.strip
+  input = gets.strip
+  trail_description(input)
+
+  if input == "list"
+    self.county_trails
+  else
+    puts "I did not recognize that command."
+    puts "To go back to the list of PBC trails, type list; or type exit"
+  end
+end
+
+
+
+def trail_description(input)
      if input == '1'
         puts "trail 1"
         puts "To go back to the list of PBC trails, type list; or type exit"
@@ -27,16 +41,8 @@ def county_trails
        puts "To go back to the list of PBC trails, type list; or type exit"
      else
        puts "I did not recognize that command."
-       puts "Please type the number of the trail you would like to know more about, or type exit."
+       county_trails
     end
-     input = gets.strip
-     if input == "list"
-       self.county_trails
-     else
-       puts "I did not recognize that command."
-       puts "To go back to the list of PBC trails, type list; or type exit"
-
-end
 end
 
 end
