4dc210294a9bf289466f00d54e7139bf00ac05f7
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-31 14:27:53 +0000

    more CLI, difficulty with the loop for CLI on first layer deep

--- lib/Park_Trails/CLI.rb
+++ lib/Park_Trails/CLI.rb
@@ -2,26 +2,41 @@ class CLI
 
 def initialize
   puts "Hello! I see you are interested in nature trails in Palm Beach County!"
+  county_trails
 end
 
 def county_trails
   puts "Here is the list of PBC trails:"
   puts "trail 1"
-       "trail 2"
-       "trail 3"
-       "trail 4"
+  puts "trail 2"
+  puts "trail 3"
+  puts "trail 4"
   puts "Please type the number of the trail you would like to know more about, or type exit."
     input = gets.strip
-     case input
-     when 1
+     if input == '1'
         puts "trail 1"
-     when 2
+        puts "To go back to the list of PBC trails, type list; or type exit"
+     elsif input == '2'
        puts "trail 2"
-     when 3
-       "trail 3"
-     when 4
-       "trail 4"
-end
+       puts "To go back to the list of PBC trails, type list; or type exit"
+     elsif input == '3'
+       puts "trail 3"
+       puts "To go back to the list of PBC trails, type list; or type exit"
+     elsif input == '4'
+       puts "trail 4"
+       puts "To go back to the list of PBC trails, type list; or type exit"
+     else
+       puts "I did not recognize that command."
+       puts "Please type the number of the trail you would like to know more about, or type exit."
+    end
+     input = gets.strip
+     if input == "list"
+       self.county_trails
+     else
+       puts "I did not recognize that command."
+       puts "To go back to the list of PBC trails, type list; or type exit"
 
+end
+end
 
 end
