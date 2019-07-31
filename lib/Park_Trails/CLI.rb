62022f2a49fe6364bb3756bd94c734dec93a549a
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-31 21:16:01 +0000

    CLI issues

--- lib/Park_Trails/CLI.rb
+++ lib/Park_Trails/CLI.rb
@@ -2,10 +2,12 @@ class CLI
 
 def initialize
   puts "Hello! I see you are interested in nature trails in Palm Beach County!"
-  county_trails
-  back_to_list
 end
 
+def run
+  trail_description
+  goodbye
+end
 
 def county_trails
   puts "Here is the list of PBC trails:"
@@ -13,49 +15,103 @@ def county_trails
   puts "trail 2"
   puts "trail 3"
   puts "trail 4"
+end
+
+def trail_description
+  county_trails
+
+  input = nil
+  while input != "exit"
+
   puts "Please type the number of the trail you would like to know more about, or type exit."
   input = gets.strip
-  trail_description(input)
-end
-
-def back_to_list
-  puts "To go back to the list of PBC trails, type list; or type exit"
-  input = gets.strip
-  if input == "list"
-    self.county_trails
-  elsif input == "exit"
-
-  else
-    puts "I did not recognize that command."
-    back_to_list
-  end
-end
-
-
-
-def trail_description(input)
-     if input == '1'
-        puts "trail 1"
-        back_to_list
-
-     elsif input == '2'
+  case input
+    when '1'
+       puts "trail 1"
+       puts "To go back to the list of PBC trails type list, or type exit"
+    when  '2'
        puts "trail 2"
-       back_to_list
-
-     elsif input == '3'
+       puts "To go back to the list of PBC trails type list, or type exit"
+    when  '3'
        puts "trail 3"
-       back_to_list
-
-     elsif input == '4'
+       puts "To go back to the list of PBC trails type list, or type exit"
+    when  '4'
        puts "trail 4"
-       back_to_list
-
-
+       puts "To go back to the list of PBC trails type list, or type exit"
+    when 'list'
+      trail_description
      else
        puts "I did not recognize that command."
-       county_trails
+       puts "Please type the number of the trail you would like to know more about, or type exit."
     end
-    back_to_list
+end
 end
 
+
+def goodbye
+  puts "Goodbye!"
+end
+# def trail_description(input)
+#     #  if input == "exit"
+#     #  puts "Goodbye"
+#
+#   when '1'
+#         puts "trail 1"
+#         back_to_list
+#
+#      when  '2'
+#        puts "trail 2"
+#        back_to_list
+#
+#     when  '3'
+#        puts "trail 3"
+#        back_to_list
+#
+#     when  '4'
+#        puts "trail 4"
+#        back_to_list
+#
+#
+#      else
+#        puts "I did not recognize that command."
+#        county_trails
+#     end
+#     back_to_list
+# end
+
+# def back_to_list
+#   input = nil
+#   while input != "exit"
+#   puts "To go back to the list of PBC trails type list, or type exit"
+#   input = gets.strip
+#   if input == "list"
+#     self.county_trails
+#   else
+#     puts "I did not recognize that command."
+#     puts "To go back to the list of PBC trails type list, or type exit"
+#   end
+# end
+# end
+
+
+
+# def trail_description(input)
+#      if input == '1'
+#         puts "trail 1"
+#         back_to_list
+#      elsif input == '2'
+#        puts "trail 2"
+#        back_to_list
+#      elsif input == '3'
+#        puts "trail 3"
+#        back_to_list
+#      elsif input == '4'
+#        puts "trail 4"
+#        back_to_list
+#      else
+#        puts "I did not recognize that command."
+#        county_trails
+#     end
+# end
+#
 end
