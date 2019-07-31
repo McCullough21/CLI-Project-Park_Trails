6c06d4d71ce1b88b566a303f06c39dbad85f2382
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-31 17:14:24 +0000

    CLI mostly done, (exit) will not terminate program

--- lib/Park_Trails/CLI.rb
+++ lib/Park_Trails/CLI.rb
@@ -3,10 +3,11 @@ class CLI
 def initialize
   puts "Hello! I see you are interested in nature trails in Palm Beach County!"
   county_trails
+  back_to_list
 end
 
+
 def county_trails
- 
   puts "Here is the list of PBC trails:"
   puts "trail 1"
   puts "trail 2"
@@ -15,12 +16,18 @@ def county_trails
   puts "Please type the number of the trail you would like to know more about, or type exit."
   input = gets.strip
   trail_description(input)
+end
 
+def back_to_list
+  puts "To go back to the list of PBC trails, type list; or type exit"
+  input = gets.strip
   if input == "list"
     self.county_trails
+  elsif input == "exit"
+
   else
     puts "I did not recognize that command."
-    puts "To go back to the list of PBC trails, type list; or type exit"
+    back_to_list
   end
 end
 
@@ -29,20 +36,26 @@ end
 def trail_description(input)
      if input == '1'
         puts "trail 1"
-        puts "To go back to the list of PBC trails, type list; or type exit"
+        back_to_list
+
      elsif input == '2'
        puts "trail 2"
-       puts "To go back to the list of PBC trails, type list; or type exit"
+       back_to_list
+
      elsif input == '3'
        puts "trail 3"
-       puts "To go back to the list of PBC trails, type list; or type exit"
+       back_to_list
+
      elsif input == '4'
        puts "trail 4"
-       puts "To go back to the list of PBC trails, type list; or type exit"
+       back_to_list
+
+
      else
        puts "I did not recognize that command."
        county_trails
     end
+    back_to_list
 end
 
 end
