bdb885aa808100a08b2832a32677ea4c8f9a4f30
Michael McCullough <michael.mccullough21@gmail.com>
2019-08-03 19:05:54 +0000

    notes for scraper class functions to be created

--- lib/Park_Trails/Scraper.rb
+++ lib/Park_Trails/Scraper.rb
@@ -1,5 +1,7 @@
 class Scraper
-def initialize
-  puts "hello"
-end
+
+# will scrape webpage for data and create hash of trail hashes
+
+#will iterate through hash of trail hashes and instantiate new trails in "Trails class" with each trail having a hash of keys and values
+
 end
