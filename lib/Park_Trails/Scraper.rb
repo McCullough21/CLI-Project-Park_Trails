3b0f63198e36754bd70be241fcd31886aef51e41
Michael McCullough <michael.mccullough21@gmail.com>
2019-08-03 19:07:54 +0000

    changed hash to array

--- lib/Park_Trails/Scraper.rb
+++ lib/Park_Trails/Scraper.rb
@@ -2,6 +2,7 @@ class Scraper
 
 # will scrape webpage for data and create hash of trail hashes
 
-#will iterate through hash of trail hashes and instantiate new trails in "Trails class" with each trail having a hash of keys and values
+#will iterate through hash of trail hashes and instantiate new trails in "Trails class" with each trail having an array of info
+
 
 end
