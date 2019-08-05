4ad5b8965958ebeb0802315ff4896c379985776d
Michael McCullough <michael.mccullough21@gmail.com>
2019-08-05 18:09:04 +0000

    issues with pry

--- lib/Park_Trails/Scraper.rb
+++ lib/Park_Trails/Scraper.rb
@@ -15,24 +15,26 @@ trails = page.css("div.col-sm-8 h2 span").each do |trail|
 if trail.text != ""
 trailss << trail.text
 end
-count = 1
-trail(count) = []
- trailss.each do |name|
+end
+trailss.each do |name|
+  trail = []
+  count = 1
    trail(count) << name
    count += 1
    @@all << trail(count)
+   binding.pry
  end
 
- end
+
 
 
 end
 
-def self.trail_info
-address = page.css("div.col-sm-7")[0].css("p").each do |info|
-puts info.text
-end
-end
+# def self.trail_info
+# address = page.css("div.col-sm-7")[0].css("p").each do |info|
+# puts info.text
+# end
+# end
 # will scrape webpage for data and create hash of trail hashes
 
 #will iterate through hash of trail hashes and instantiate new trails in "Trails class" with each trail having an array of info
