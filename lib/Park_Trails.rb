27da61f9d4c7dd41d67624ff52fa4d9c0209788d
Michael McCullough <michael.mccullough21@gmail.com>
2019-08-05 15:20:42 +0000

    scraper, was able to scrape park names

--- lib/Park_Trails.rb
+++ lib/Park_Trails.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+require 'open-uri'
 require_relative "./Park_Trails/version"
 require_relative "./Park_Trails/CLI"
 require_relative "./Park_Trails/Scraper"
--- lib/Park_Trails/Scraper.rb
+++ lib/Park_Trails/Scraper.rb
@@ -1,19 +1,26 @@
-require 'Nokogiri'
+require 'nokogiri'
 require 'open-uri'
-
+require 'pry'
 
 class Scraper
 
 @@all = []
 
-# def self.scrape
-#
-#   trail = []
-#
-# doc = Nokogiri::HTML(open("http://discover.pbcgov.org/parks/Pages/NatureTrails.aspx"))
-# trail << doc.css("div.col-sm-8.h2.span").text
-#
-# end
+def self.scrape
+trail = []
+
+doc = Nokogiri::HTML(open("http://discover.pbcgov.org/parks/Pages/NatureTrails.aspx"))
+trail << doc.css("div.col-sm-8.h2.span").text
+binding.pry
+end
+
+page = Nokogiri::HTML(open(site))
+trailss = []
+trails = page.css("div.col-sm-8 h2 span").each do |trail|
+if trail.text != ""
+trailss << trail.text
+end
+
 # will scrape webpage for data and create hash of trail hashes
 
 #will iterate through hash of trail hashes and instantiate new trails in "Trails class" with each trail having an array of info
