3074f34c4a663013dcf51f742be8ecd9f689b0fb
Michael McCullough <michael.mccullough21@gmail.com>
2019-08-06 23:53:12 +0000

    format code

--- lib/Park_Trails/Scraper.rb
+++ lib/Park_Trails/Scraper.rb
@@ -9,37 +9,37 @@ class Scraper
 @@all = []
 attr_accessor :name, :address, :hours, :description
 
-def self.trail_names
+def self.trails
 
   page = Nokogiri::HTML(open("http://discover.pbcgov.org/parks/Pages/NatureTrails.aspx"))
   trailss = []
   trails = page.css("div.ms-webpart-zone.ms-fullWidth div.s4-wpcell-plain.ms-webpartzone-cell.ms-webpart-cell-vertical.ms-fullWidth")
   trails.each do |trail|
-if trail.css("h2 span").text != "" && trail.css("h2 span").text != nil
-     @name = trail.css("h2 span").text
-   info = trail.css("p")
+    if trail.css("h2 span").text != "" && trail.css("h2 span").text != nil
+      @name = trail.css("h2 span").text
+      info = trail.css("p")
       info.each do |gems|
         if gems.text.include?("Entrance") && gems.text != nil
           @address = gems.text.strip!
         elsif gems.text.include?("Sunrise")
            @hours = gems.text.strip!
-             if @hours.include?("here")
-               29.times do
-                 @hours.chop!
-               end
-             end
-             if @hours.include?("sunsetC")
+           if @hours.include?("here")
+              29.times do
+                @hours.chop!
+              end
+           end
+           if @hours.include?("sunsetC")
                @hours.chop!
-             end
+           end
 
-        info[-1].text != "​R​eturn to Nature​"
-       @description = info[-1].text.strip!
-       Trails.new(@name, @address, @hours, @description)
+          info[-1].text != "​R​eturn to Nature​"
+          @description = info[-1].text.strip!
+          Trails.new(@name, @address, @hours, @description)
         end
+      end
+    end
 
-   end
-end
-end
+  end
 binding.pry
 end
 
